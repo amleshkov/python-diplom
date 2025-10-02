9198cc91689e30d4ec48bc25c9b59172d4c69b1b
Aleksandr Leshkov <lam@arenadata.io>
2025-10-02 20:24:59 +0300

    Ah, another attempt to struggle with pytest

--- orders/tests/test_backend.py
+++ orders/tests/test_backend.py
@@ -1,6 +1,26 @@
 import pytest
+from rest_framework.test import APIClient
 
-from .conftest import USER_REGISTER_URL
+USER_REGISTER_URL = "/api/v1/user/register"
+USER_CONFIRM_URL = "/api/v1/user/register/confirm"
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def user(client):
+    data = {
+        "first_name": "John",
+        "last_name": "Doe",
+        "email": "johndoe@example.com",
+        "password": "onsUbPiOc@",
+        "company": "Acme Inc.",
+        "position": "Sales Manager",
+    }
+    client.post(USER_REGISTER_URL, data)
 
 
 @pytest.mark.django_db
