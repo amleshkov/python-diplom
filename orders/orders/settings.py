ab4cccac17698a9cf45e27873fa8eb592009a964
Aleksandr Leshkov <lam@arenadata.io>
2025-10-02 15:02:42 +0300

    Create and apply migrations (and some fixes)

--- orders/orders/settings.py
+++ orders/orders/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "django_rest_passwordreset",
+    "backend.apps.BackendConfig",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +132,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "backend.User"
